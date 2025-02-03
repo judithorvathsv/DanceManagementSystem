f1d76194494567647febb7a9595357d1f9b5aa52
Judit Horvath <judit.horvath@appliedtechnology.se>
2025-02-03 16:16:40 +0100

    fix: index style on mobile frontend

--- Frontend/src/routes/index.tsx
+++ Frontend/src/routes/index.tsx
@@ -7,14 +7,16 @@ export const Route = createFileRoute("/")({
 
 function RouteComponent() {
   return (
-<div className="home">
-  <div className="home_container">
-    <div className="home_image"></div>
-    <section className="home_section">
-      <h2 className="text-3xl mb-24">Discover Your Passion for Dance!</h2>
-      <Register/>
-    </section>
-  </div>
-</div>
+    <div className="home">
+      <div className="home_container">
+        <div className="home_image"></div>
+        <section className="home_section w-full px-4 sm:px-0 sm:w-auto">
+          <h2 className="text-3xl mb-12 sm:mb-24">
+            Discover Your Passion for Dance!
+          </h2>
+          <Register />
+        </section>
+      </div>
+    </div>
   );
 }
